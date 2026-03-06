0562252167e1a087bd97a7f2b36358728c0f472d
somewhereMaDan <madanmohan7272906844@gmail.com>
2026-03-06 20:21:57 +0530

    ..

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const express = require('express');
 const app = express();
 
 const corsOptions = {
-  origin: ['http://localhost:5173', 'https://kraft-venture.vercel.app'],
+  // origin: ['http://localhost:5173', 'https://kraft-venture.vercel.app'],
+  origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 };
